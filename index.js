60c9e1fbbe55e10ef6b62d37deb56b85ff95705d
Mixtirio <52823589+Mixtirio@users.noreply.github.com>
2022-03-26 22:47:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ fs.readdir('./commands', (err, files) => {
 bot.on('ready', async () => {
     console.log('Bot En Ligne')
     bot.user.setStatus("online");
-    bot.user.setActivity('-help | Mange des ramens.', {type : 'STREAMING'})
+    bot.user.setActivity('-help | Mange des ramens.', {type: 'STREAMING'})
 })
 
 
