5201b9bc1aebf2c1c92904e03be86e72a3f428f5
Mixtirio <52823589+Mixtirio@users.noreply.github.com>
2022-03-27 16:48:50 +0200

    Update nitro.js

--- commands/nitro.js
+++ commands/nitro.js
@@ -5,8 +5,8 @@ module.exports = {
         const member = message.mentions.members.first()
         if (!message.member.permissions.has('ADMINISTRATOR')) return message.channel.send('Vous n\'avez pas la permission d\'utiliser cette commande.')
         const nitro = 'https://discord.gift/'
-        range(16)
-        const caractère  = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9",]
+        in range(16)
+            const caractère  = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9",]
 
         message.channel.send(nitro+caractère[Math.floor(Math.random()*imgslap.length)])
 
